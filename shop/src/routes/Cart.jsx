ec95345c87348d0f22b5276051626c4d755ea4b7
Sammyissewon <person1341@gmail.com>
2024-07-03 23:21:19 +0700

    redux 3

--- shop/src/routes/Cart.jsx
+++ shop/src/routes/Cart.jsx
@@ -2,22 +2,16 @@ import { Table } from "react-bootstrap";
 import { useSelector } from "react-redux";
 
 function Cart() {
-  // useSelector: redux store을 가져와줌
-  // useSelector(함수 왜 넣니?): 그냥 넣어
-  // (state): store.js에 저장된 모든 state를 의미
-  // (state) => state.stock2 식으로, 특정 state만 불러올 수 있음
-  let state = useSelector((state) => state.stock2);
-  console.log(state);
+  let a = useSelector((state) => state);
+  console.log(a.cart);
 
   return (
     <div>
       <Table>
         <thead>
-          {/* 가로칸 생성 */}
           <tr>
-            {/* 세로칸 생성 */}
             <th>#</th>
-            <th>{state}</th>
+            <th>{a.cart}</th>
             <th>수량</th>
             <th>변경하기</th>
           </tr>
--- shop/src/store.js
+++ shop/src/store.js
@@ -1,26 +1,22 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-// state 만들기, state 1개 = slice
-// createSlice({
-//   name: "state이름",
-//   initialState: "값",
-// });
-
 let user = createSlice({
   name: "user",
   initialState: "lee",
 });
 
-let stock2 = createSlice({
-  name: "stock2",
-  initialState: [10, 11, 12],
+let cart2 = createSlice({
+  name: "cart",
+  initialState: [
+    { id: 0, name: "White and Black", count: 2 },
+    { id: 2, name: "Grey Yordan", count: 1 },
+  ],
 });
 
+// 여기에 slice(state)를 등록해야 함. state명.reducer
 export default configureStore({
-  // 여기에 slice(state)를 등록해야 함. state명.reducer
   reducer: {
-    // 작명: 변수명.reducer
     user: user.reducer,
-    stock2: stock2.reducer,
+    cart: cart2.reducer,
   },
 });
