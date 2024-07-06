516f1f70892f45906e8f892cbd99fac9d3491d1e
Sammyissewon <person1341@gmail.com>
2024-07-06 17:54:07 +0700

    tic_tac_toe 1: div, css

---
+++ tic_tac_toe/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import TicTacToe from "./components/TicTacToe/TicTacToe";
+
+function App() {
+  return (
+    <>
+      <TicTacToe />
+    </>
+  );
+}
+
+export default App;
