68e49f99f5cd061210da55bac16882f856c37380
Sammyissewon <person1341@gmail.com>
2024-07-07 16:15:44 +0700

    to_do_list2: 기능구현2. useState, useRef

--- to_do_list2/src/App.jsx
+++ to_do_list2/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Header from "./Components/Header";
 import Editor from "./Components/Editor";
 import List from "./Components/List";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 // 1. UI 짜기
 // 2. 각 컴포넌트의 기본 CSS 설정
@@ -31,13 +31,30 @@ const mockData = [
 ];
 
 function App() {
-  // List 입력값을 업데이트하는 state
+  // 12. List 입력값을 업데이트하는 state
   const [todos, setTodos] = useState([mockData]);
+
+  // 23. 목업데이터의 id가 0~2이어서, 새로 입력한 값은 id: 3부터 시작함. 따라서 useRef의 초기값은 3
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    // 13. 새로운 입력값을 객체형태로 만들기
+    const newTodo = {
+      // 24. 초기값 3부터 시작해서 1씩 증가하는 id
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+    // 14. 기존 todos 객체배열을 스프레드 연산자로 불러와서, 앞에 새로운 입력값(newTodo)를 추가
+    setTodos([newTodo, ...todos]);
+  };
 
   return (
     <div className="App">
       <Header />
-      <Editor />
+      {/* 15. Editor 컴포넌트에 함수 props 전달 */}
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
--- to_do_list2/src/Components/Editor.jsx
+++ to_do_list2/src/Components/Editor.jsx
@@ -1,11 +1,53 @@
 import "./Editor.css";
+import { useState, useRef } from "react";
 
-const Editor = () => {
+// 16. 함수 props 받아와서
+const Editor = ({ onCreate }) => {
+  // 18. 매번 입력창에 입력한 값을 state로 저장
+  const [content, setContent] = useState("");
+  // 19. input에 입력한 값으로 content state 업데이트 하는 로직
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  // 26. 빈값인데 버튼 입력하면 input창에 focus 효과주기 위함
+  const contentRef = useRef();
+
+  // 22. 버튼을 누르면 입력한 값을 파라미터 삼아서 onCreate 함수(App.jsx)를 실행
+  const onSubmit = () => {
+    // 25. 버튼 눌렀는데, 빈값이라면 onCreate 실행하지 말고 바로 return 종료
+    if (content == "") {
+      // 28. 입력창이 빈칸시 focus 효과 적용하기
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    // 29. 매번 입력버튼 누르고나서 입력창 비우기
+    setContent("");
+  };
+
+  // 30. 버튼 누르기 외에, 엔터키 눌러도 실행되는 로직
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
   return (
     // 5. 디자인하기
     <div className="Editor">
-      <input placeholder="New To-do"></input>
-      <button>Add To-do</button>
+      <input
+        // 27. 입력창이 빈칸시 focus 효과 적용하기
+        ref={contentRef}
+        value={content}
+        // 20. 뭔가를 입려하면 setContet 변경함수를 실행
+        onChange={onChangeContent}
+        // 31. 엔터기로 실행하기
+        onKeyDown={onKeydown}
+        placeholder="New To-do"
+      ></input>
+      {/* 21. 버튼을 누르면 onSubmit 함수 실행 */}
+      <button onClick={onSubmit}>Add To-do</button>
     </div>
   );
 };
