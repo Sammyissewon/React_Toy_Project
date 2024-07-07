9064d17c1fd57871799d2c304959709b6e5fb4dd
Sammyissewon <person1341@gmail.com>
2024-07-07 17:27:56 +0700

    to_do_list2: 기능구현: 서치바 기능 추가

--- to_do_list2/src/Components/List.jsx
+++ to_do_list2/src/Components/List.jsx
@@ -5,20 +5,54 @@
 
 import "./List.css";
 import TodoItem from "./TodoItem";
+import { useState } from "react";
 
 const List = ({ todos }) => {
+  // 37. To-do 검색창 로직
+  const [search, setSearch] = useState("");
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  // 39. 서치창에 입력한 값으로 필터링 리스트 필터링하기
+  const getFilteredData = () => {
+    // 40. 서치창 빈값이면, 리스트 모두 반환
+    if (search === "") {
+      return todos;
+    }
+    // 41. 서치창에 입력값을 포함한 값들만 걸러서 반환
+    // 43. true인 값들만 filter해서 반환
+    return todos.filter((todo) => {
+      // 42. content가 서치 입력창 내용을 includes하면 true
+      // 45. 대소문자 관계 없이 검색되도록, 소문자 전환 메서드 사용
+      todo.content.toLowerCase().includes(search.toLowerCase());
+    });
+  };
+
+  // 44. 서치창 필터링함수를 변수로 지정
+  const filteredTodos = getFilteredData();
+
   return (
     <div className="List">
       <h4>To-do list</h4>
-      <input placeholder="What to search"></input>
+      <input
+        value={search}
+        // 38. 유저가 서치창에 입력하면, 그 입력값이 search state에 저장됨
+        onChange={onChangeSearch}
+        placeholder="What to search"
+      ></input>
+
       <div className="todos_wrapper">
         {/* 32. todos 배열 state를 map 돌려서 */}
         {/* 33. 각 배열의 요소를 todo로 받고 */}
-        {todos.map((todo) => {
+        {/* 45. 필터링된 값으로 map 돌리기 */}
+        {filteredTodos.map((todo) => {
           // 34. 콜백함수(return)으로 요소 갯수만큼 반환하기
           return (
             <div>
-              <TodoItem {...todo} />
+              {/* 35. 콜백함수는 컴포넌트{...props} 형태를 받을 수 있다 */}
+              {/* 36. map 등으로 만들어진 배열의 요소들은 고유의 key(id)를 가져야함 */}
+              <TodoItem key={todo.id} {...todo} />
             </div>
           );
         })}
--- to_do_list2/src/Components/TodoItem.jsx
+++ to_do_list2/src/Components/TodoItem.jsx
@@ -2,12 +2,15 @@
 
 import "./TodoItem.css";
 
-const TodoItem = () => {
+// 36. App.js -> List -> TodoItem으로 전달된 props를 분해할당하기
+const TodoItem = ({ id, isDone, content, date }) => {
   return (
     <div className="TodoItem">
-      <input type="checkbox" />
-      <div className="content">To-do...</div>
-      <div className="date">Date...</div>
+      {/* 38. checked라는 props명은 정의하지 않았어서 콘솔에 에러 방출 -> readOnly로 임시 해결 */}
+      <input readOnly checked={isDone} type="checkbox" />
+      <div className="content">{content}</div>
+      {/* 37. date 양식 변환 */}
+      <div className="date">{new Date(date).toLocaleDateString()}</div>
       <button>Delete</button>
     </div>
   );
