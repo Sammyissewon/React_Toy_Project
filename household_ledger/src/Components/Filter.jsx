86c8d61fde85da66c0fe5a9191cd65d02d52e4de
Sammyissewon <person1341@gmail.com>
2024-07-08 22:10:28 +0700

    List, Filter UI

--- household_ledger/src/Components/Filter.jsx
+++ household_ledger/src/Components/Filter.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Filter = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          유형 필터
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">식료품</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">생필품</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">교통비</Dropdown.Item>
+          <Dropdown.Item href="#/action-4">공과금</Dropdown.Item>
+          <Dropdown.Item href="#/action-5">사치품</Dropdown.Item>
+          <Dropdown.Item href="#/action-6">적금</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          정렬 기준
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">가격 높은 순</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">가격 낮은 순</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">최신 순</Dropdown.Item>
+          <Dropdown.Item href="#/action-4">오래된 순</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
+      {/* 기간 범위 */}
+    </div>
+  );
 };
 
 export default Filter;
--- household_ledger/src/Components/List.jsx
+++ household_ledger/src/Components/List.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const List = () => {
-  return <div></div>;
+  return (
+    <div>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">날짜</th>
+            <th scope="col">유형</th>
+            <th scope="col">이름</th>
+            <th scope="col">가격</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>date</td>
+            <td>식료품</td>
+            <td>쌀</td>
+            <td>5000</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default List;
