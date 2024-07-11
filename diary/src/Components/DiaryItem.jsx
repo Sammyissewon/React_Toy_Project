22103fd0709d6933215449a9f4a9fc3d8c0cfa85
Sammyissewon <person1341@gmail.com>
2024-07-11 11:18:14 +0700

    Home 페이지 추가 css

--- diary/src/Components/DiaryItem.jsx
+++ diary/src/Components/DiaryItem.jsx
@@ -3,15 +3,22 @@ import React from "react";
 import { getEmotionImage } from "../util/get-emotion-image";
 import Button from "./Button";
 
+import "./DiaryItem.css";
+
 const DiaryItem = () => {
+  // 이모션 이미지의 배경색 동적으로 관리하기
+  const emotionId = 1;
+
   return (
     <div className="DiaryItem">
-      <div className="img_section">
-        <img src={getEmotionImage(1)} />
+      {/* // 이모션 이미지의 배경색 동적으로 관리하기 */}
+      <div className={`img_section img_section_${emotionId}`}>
+        {/* // 이모션 이미지의 배경색 동적으로 관리하기 */}
+        <img src={getEmotionImage(emotionId)} />
       </div>
       <div className="info_section">
-        <div className="create_date">{new Date().toLocaleDateString()}</div>
-        <div>일기 컨텐츠</div>
+        <div className="created_date">{new Date().toLocaleDateString()}</div>
+        <div className="created_content">일기 컨텐츠</div>
       </div>
       <div className="button_section">
         <Button text={"수정하기"} />
