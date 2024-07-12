3d7602d9c48821189c1640530651d764eec504b2
Sammyissewon <person1341@gmail.com>
2024-07-12 20:45:09 +0700

    state 작성

--- diary/src/Components/Editor.jsx
+++ diary/src/Components/Editor.jsx
@@ -4,6 +4,8 @@ import "./Editor.css";
 import EmotionItem from "./EmotionItem";
 import Button from "./Button";
 
+import { useState } from "react";
+
 // 이모티콘 5개를 하드코딩 하지말고, 컴포넌트 외부에 배열로 선언하고 map 돌리기
 const emotionList = [
   {
@@ -28,15 +30,60 @@ const emotionList = [
   },
 ];
 
+// 날짜 -> YYYY-MM-DD로 변환하는 함수
+// 그냥 new Date()를 쓰면, 오늘의 날짜 value가 인식하지 못함. 문자열로 변환해서 전달해야 함
+// 따라서 input.createdDate를 getStringedDate 함수로 돌려줘야 함
+const getStringedDate = (targetDate) => {
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() + 1;
+  let date = targetDate.getDate();
+
+  // 월, 일이 1자리 수일때, 앞에 0을 붙여주는 로직
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  if (date < 10) {
+    date = `0${date}`;
+  }
+  return `${year}-${month}-${date}`;
+};
+
 const Editor = () => {
+  // 오늘의 날짜, 감정, 일기내용 저장 state
+  const [input, setInput] = useState({
+    createdDate: new Date(),
+    emotionId: 3,
+    content: "",
+  });
+
+  // 오늘의 날짜, 감정, 일기내용 state 변경 이벤트 로직
+  const onChangeInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    // 날짜를 바꾼거라면, 날짜(현재 문자열)을 new Date로 한번 변환시켜줘야함
+    if (name === "createdDate") {
+      value = new Date(value);
+    }
+
+    // input state가 배열형태이기 때문에, 스프레드 연산자로 가져와서 이벤트가 실행된 name의 값만 업데이트 하는 형태
+    setInput({ ...input, [name]: value });
+  };
+
   const emotionId = 5;
 
   return (
     <div className="Editor">
       <section className="date_section">
         <h4>오늘의 날짜</h4>
-        <input type="date" />
+        <input
+          name="createdDate"
+          onChange={onChangeInput}
+          value={getStringedDate(input.createdDate)}
+          type="date"
+        />
       </section>
+
       <section className="emotion_section">
         <h4>오늘의 감정</h4>
         <div className="emotion_list_wrapper">
@@ -53,10 +100,12 @@ const Editor = () => {
           ))}
         </div>
       </section>
+
       <section className="content_section">
         <h4>오늘의 일기</h4>
         <textarea placeholder="오늘은 어땠나요?"></textarea>
       </section>
+
       <section className="button_section">
         <Button text={"취소하기"} />
         <Button text={"작성완료"} type={"POSITIVE"} />
--- diary/src/pages/New.jsx
+++ diary/src/pages/New.jsx
@@ -4,12 +4,24 @@ import Header from "../Components/Header";
 import Button from "../Components/Button";
 import Editor from "../Components/Editor";
 
+import { useNavigate } from "react-router-dom";
+
 const New = () => {
+  const nav = useNavigate();
+
   return (
     <div>
       <Header
         title={"새 일기 쓰기"}
-        leftChild={<Button text={"< 뒤로 가기"} />}
+        leftChild={
+          <Button
+            text={"< 뒤로 가기"}
+            onClick={() => {
+              // -1하면 뒤로감
+              nav(-1);
+            }}
+          />
+        }
       />
       <Editor />
     </div>
