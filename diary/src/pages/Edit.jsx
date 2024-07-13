d47c92d2a68741f0fe0d01eba7342ea34f50327c
Sammyissewon <person1341@gmail.com>
2024-07-13 11:25:54 +0700

    Edit 기능 구현: 버튼 추가

--- diary/src/pages/Edit.jsx
+++ diary/src/pages/Edit.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import Header from "../Components/Header";
+import Button from "../Components/Button";
+import Editor from "../Components/Editor";
 
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 const Edit = () => {
   const params = useParams();
-  return <div>{params.id}번 일기 수정 페이지입니다.</div>;
+  const nav = useNavigate();
+
+  // 삭제하기 버튼 눌렀을 때 추가확인알림 표시
+  const onClickDelete = () => {};
+
+  return (
+    <div>
+      <Header
+        title="일기수정하기"
+        leftChild={
+          <Button
+            onClick={() => {
+              nav(-1);
+            }}
+            text={"< 뒤로가기"}
+          />
+        }
+        rightChild={
+          <Button onClick={onClickDelete} text={"삭제하기"} type={"NEGATIVE"} />
+        }
+      />
+      <Editor />
+    </div>
+  );
 };
 
 export default Edit;
